f9abc39eb12c81cfae9a766ece0e010edb69618e
Mohnish4533 <mv4533@srmist.edu.in>
2023-06-25 13:53:07 +0530

    Image based Object Detection program

---
+++ ImageDetection.py
@@ -0,0 +1,91 @@
+import os
+import cv2 
+import numpy as np
+from matplotlib import pyplot as plt
+
+import object_detection
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+
+#from object_detection.protos import pipeline_pb2
+#from google.protobuf import text_format
+
+
+
+CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
+PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
+LABEL_MAP_NAME = 'label_map.pbtxt'
+
+paths = {
+    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
+    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
+    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport')
+ }
+
+files = {
+    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+}
+
+
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-4')).expect_partial()
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+
+category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
+
+IMAGE_PATH = os.path.join(paths['IMAGE_PATH'], 'test', 'R_1100.jpg')
+
+
+img = cv2.imread(IMAGE_PATH)
+image_np = np.array(img)
+
+input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+detections = detect_fn(input_tensor)
+
+num_detections = int(detections.pop('num_detections'))
+detections = {key: value[0, :num_detections].numpy()
+              for key, value in detections.items()}
+detections['num_detections'] = num_detections
+
+# detection_classes should be ints.
+detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+label_id_offset = 1
+image_np_with_detections = image_np.copy()
+
+viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes']+label_id_offset,
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=5,
+            min_score_thresh=.8,
+            agnostic_mode=False)
+
+plt.imshow(cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB))
+plt.savefig("TEST.jpg")
+#plt.show()
